3ca9b7bf89e49be5481d68c34923017d7a86a1be
Dexter Dow <15382779+dexterdow33@users.noreply.github.com>
2018-09-15 13:53:35 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+// Blog Post Previewer 
+
+// const title = "My First Blog Post";
+// const content = "This is my first post!";
+
+  $('#flashMessage')
+  .hide()
+
+$('#previewButton').click(function(){
+  const title = $('#blogTitleInput').val();
+  const content = $('#blogContentInput').val();
+  
+  $('#blogTitlePreview').text(title);
+  $('#blogContentPreview').text(content);
+  
+  $('#flashMessage')
+  .slideDown(1000)
+  .delay(3000)
+  .slideUp();
+
+});
